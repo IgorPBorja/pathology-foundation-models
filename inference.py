2c34af82d80cb67fbcea32614b3f4cd1517dfa59
IgorPBorja <igorpradoborja@gmail.com>
2025-07-27 00:02:47 +0000

    fix: fix non-interactive login to HF API

--- inference.py
+++ inference.py
@@ -1,28 +1,62 @@
 import torch
-import PIL
+import torchvision.transforms as T
+
+from typing import Literal
+from PIL.Image import Image
 
 from .loader import FoundationModel
 
 
-def extract_features_from_pil(image: PIL.Image, model: FoundationModel) -> torch.Tensor:
+def convert_to_batch_tensor(image: Image | torch.Tensor) -> torch.Tensor:
+    """
+    Converts a single PIL image or a torch tensor of shape (3, H, W) to a batch tensor of shape (1, 3, H, W).
+
+    If the input is already a batch tensor of shape (N, 3, H, W), it will be returned as is.
+
+    :param image: PIL Image or torch.Tensor of shape (3, H, W) or torch Tensor of shape (N, 3, H, W)
+    :return: Batch tensor of shape (1, 3, H, W)
+    """
+    if isinstance(image, Image):
+        transform = T.ToTensor()
+        image = transform(image).unsqueeze(0)  # Add batch dimension
+    elif isinstance(image, torch.Tensor):
+        if image.dim() == 3:
+            image = image.unsqueeze(0)  # Add batch dimension
+        elif image.dim() != 4:
+            raise ValueError("Input tensor must be of shape (3, H, W) or (N, 3, H, W)")
+    else:
+        raise TypeError("Input must be a PIL Image or a torch Tensor")
+
+    return image
+
+
+def extract_features(
+    image: Image | torch.Tensor, model: FoundationModel, type: Literal["pil", "torch"]
+) -> torch.Tensor:
     """
     Extracts features from single PIL image using the specified model.
 
-    :param image: PIL Image
+    :param image: PIL Image or torch.Tensor of shape (3, H, W) or torch Tensor of shape (N, 3, H, W)
+    :param format: either 'pil' or 'torch'
     :param model: FoundationModel instance containing the model and processor
     :return: Extracted features as a torch.Tensor of shape (1, N)
     """
+    if type == "pil" and not isinstance(image, Image):
+        raise TypeError("Expected a PIL Image when type is 'pil'")
+    if type == "torch" and not isinstance(image, torch.Tensor):
+        raise TypeError("Expected a torch Tensor when type is 'torch'")
+
+    image_tensor = convert_to_batch_tensor(image)
+
     if model.model_id == "MahmoodLab/UNI" or model.model_id == "MahmoodLab/UNI2-h":
         # For UNI and UNI2-h, we use the transform directly
-        transform = model.processor
-        image = transform(image).unsqueeze(0)  # Add batch dimension
-        image = image.to(model.device)
+        image_tensor = model.processor(image_tensor).unsqueeze(0)  # Add batch dimension
+        image_tensor = image_tensor.to(model.device)
         with torch.inference_mode():
-            features = model.model(image)
+            features = model.model(image_tensor)
         return features
     elif model.model_id == "owkin/phikon-v2":
-        # Process the image
-        inputs = model.processor(image, return_tensors="pt")
+        inputs = model.processor(image_tensor, return_tensors="pt")
         inputs = {k: v.to(model.device) for k, v in inputs.items()}
         # Get the features
         with torch.inference_mode():
