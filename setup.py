26ced36286dd31fe630e33586200a9ffd7580dbc
IgorPBorja <igorpradoborja@gmail.com>
2025-07-29 22:33:23 +0000

    fix: bug in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 with open(
-    Path(__file__).parent / "dependencies" / "frozen-requirements.txt",
+    Path(__file__).parent / "dependencies" / "frozen_requirements.txt",
     "r",
     encoding="utf-8",
 ) as fh:
